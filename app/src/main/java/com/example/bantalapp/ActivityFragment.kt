1ba9be2567a7d9ac4e51c9bee86b80121dcdb56c
MuhFauzan07 <maulada2017@gmail.com>
2024-12-18 19:05:38 +0700

    Settings profile

--- app/src/main/java/com/example/bantalapp/ActivityFragment.kt
+++ app/src/main/java/com/example/bantalapp/ActivityFragment.kt
@@ -5,14 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contohrv2.ProdukAdapter
 
 class ActivityFragment : Fragment() {
+
+    lateinit var rvProduk: RecyclerView
+    var namaProduk = ArrayList<String>()
+    var hargaProduk = ArrayList<String>()
+    var gambarProduk = ArrayList<Int>()
+    var deskripsi = ArrayList<String>()
+
+    lateinit var adapterProduk: ProdukAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity, container, false)
+        val view = inflater.inflate(R.layout.fragment_activity, container, false)
+
+        // Bind RecyclerView with findViewById
+        rvProduk = view.findViewById(R.id.rvProduk)
+
+        rvProduk.layoutManager = LinearLayoutManager(requireContext())
+        namaProduk.add("Meditasi")
+        namaProduk.add("Olahraga")
+        namaProduk.add("Teh")
+
+        hargaProduk.add("Ayo lakukan Meditasi sekarang!")
+        hargaProduk.add("Jika dirimu ")
+        hargaProduk.add("5.000")
+
+        gambarProduk.add(R.drawable.susu)
+        gambarProduk.add(R.drawable.kopi)
+        gambarProduk.add(R.drawable.teh_tawar)
+        deskripsi.add("Test Ini untuk deskripsi")
+        deskripsi.add("Test Ini untuk deskripsi")
+        deskripsi.add("Test Ini untuk deskripsi")
+
+        adapterProduk = ProdukAdapter(namaProduk, hargaProduk, gambarProduk, requireContext(),  deskripsi)
+        rvProduk.adapter = adapterProduk
+
+        return view
     }
 }
---
+++ app/src/main/java/com/example/bantalapp/ProdukAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.contohrv2
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bantalapp.R
+
+class ProdukAdapter (
+    var namaProduk: ArrayList<String>,
+    var hargaProduk: ArrayList<String>,
+    var gambarProduk: ArrayList<Int>,
+    var context: Context,
+    var deskripsi: ArrayList<String>
+): RecyclerView.Adapter<ProdukAdapter.CountryViewHolder>() {
+
+    class CountryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        var tvNamaProduk: TextView = itemView.findViewById(R.id.tvNamaProduk)
+        var tvHargaProduk: TextView = itemView.findViewById(R.id.tvHargaProduk)
+        var imageView: ImageView= itemView.findViewById(R.id.ivProduk)
+        var tvDeskripsi: TextView = itemView.findViewById(R.id.tvDeskripsi)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.produk_card, parent, false)
+        return CountryViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+
+        return namaProduk.size
+    }
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+
+        holder.tvNamaProduk.text = namaProduk.get(position)
+        holder.tvHargaProduk.text = hargaProduk.get(position)
+        holder.imageView.setImageResource(gambarProduk.get(position))
+        holder.tvDeskripsi.text = deskripsi.get(position)
+    }
+
+
+}
